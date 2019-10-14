88d184efaf75a02959f6d822f924245b94590cb5
rishika3 <53818252+rishika3@users.noreply.github.com>
2019-10-14 19:26:16 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,13 @@
+import string
+>>> import random
+>>> str1=string.printable
+>>> print(str1)
+0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 	
+ 
+>>> print(len(str1))
+100
+>>> for i in range(6):
+	print(str1[randint(0,100)],end="")
+
+	
+6 =aS#2
